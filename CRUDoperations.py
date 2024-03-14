f34718982e23f8f5c55617c3f648ca8940d64007
Amasio <reventonretro@gmail.com>
2024-03-14 21:13:56 +0100

    first commit

---
+++ CRUDoperations.py
@@ -0,0 +1,48 @@
+#credential test
+import security_access_key
+import boto3
+
+session = boto3.Session(
+    aws_access_key_id=security_access_key.aws_access_key_id,
+    aws_secret_access_key=security_access_key.aws_secret_access_key,
+    region_name=security_access_key.region_name
+)
+
+s3 = session.resource('s3')
+
+#il nome dei bucket deve essere tutto in minuscolo
+bucket_name="altro-bucket-da-boto3"
+all_my_bycket=[bucket.name for bucket in s3.buckets.all()]
+
+######VERIFICA E CREAZIONE DEL BUCKET ########
+if bucket_name not in all_my_bycket:
+	print(f"'{bucket_name}' bucket dose not exist, creating...")
+	s3.create_bucket(Bucket=bucket_name)
+	print(f"'{bucket_name}' è stato creato")
+else:
+	print("bucket gia esistente")
+
+#######Creation and upload of files.#######
+file1= 'file1.txt'
+lista_file = ['file2.txt', 'file3.txt']  # e così via fino a file10.txt
+
+#upload
+s3.Bucket(bucket_name).upload_file(Filename=file1, Key=file1)
+#in key indichi dove e come vuoi che il seguente file venga salvato
+#s3.Bucket(bucket_name).upload_file(filename=file1, key=pathInbucket/file_1_with_another_name)
+for file_name in lista_file:
+    s3.Bucket(bucket_name).upload_file(Filename=file_name, Key='ListDirectory/'+ file_name)
+
+#READ
+obj=s3.Object(bucket_name, file1) #1) crei il riferimento
+response = obj.get()# 2) recuperi l' oggetto in se tramite API GET.(ovvero un file json con vari dati della richiesta +body)
+body = response['Body'].read()#3)accediamo al body
+#oppure con singolo comando
+onelinebody=obj.get()['Body'].read()
+print(body)
+
+#update file1 with the contenet of file2
+s3.Object(bucket_name, file1).put(Body=open(file2, 'rb'))
+onelinebody=obj.get()['Body'].read()
+print(onelinebody)
+
